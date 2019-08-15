ad9c24bce779564eb53cc9f1458258d802e67779
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-08-15 11:00:06 -0700

    added snippet data and displayed with highlightjs

---
+++ index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import SNIPS from './snippet-data.js';
+
+const renderSnips = snippets => {
+  // map over the SNIPS
+  // transform that snip into the HTML
+  const snippetHTML = snippets.map(
+    snippet => /* html */ `
+      <div class="snip">
+        <div class="text">
+          <h2>${snippet.title}</h2>
+          <p>${snippet.description}</p>
+        </div>
+        <pre><code class="${snippet.language}">${he.encode(snippet.code)}</code></pre>
+      </div>
+    `,
+  );
+  const snippetHTMLStrings = snippetHTML.join('');
+  // put that HTML right into the #snippets
+  const snippetsElem = document.getElementById('snippets');
+  snippetsElem.innerHTML = snippetHTMLStrings;
+};
+
+renderSnips(SNIPS);
+hljs.initHighlightingOnLoad();

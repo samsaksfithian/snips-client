b25540c2bcf75dc21db02ab6058330a3e00f8833
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-08-19 08:57:03 -0700

    some styling and organization

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 /* eslint-disable no-undef */
 import SNIPS from './snippet-data.js';
-
-const renderSnips = snippets => {
-  // map over the SNIPS
-  // transform that snip into the HTML
-  const snippetHTML = snippets.map(
-    snippet => /* html */ `
-      <div class="snip">
-        <div class="text">
-          <h2>${snippet.title}</h2>
-          <p>${snippet.description}</p>
-        </div>
-        <pre><code class="${snippet.language}">${he.encode(snippet.code)}</code></pre>
-      </div>
-    `,
-  );
-  const snippetHTMLStrings = snippetHTML.join('');
-  // put that HTML right into the #snippets
-  const snippetsElem = document.getElementById('snippets');
-  snippetsElem.innerHTML = snippetHTMLStrings;
-};
+import { renderSnips } from './lib/snippets.js';
+import search from './lib/search.js';
 
 renderSnips(SNIPS);
 hljs.initHighlightingOnLoad();
